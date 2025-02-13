83864b868859df8f20b2c132d161e2f5a6b68f40
Phenomenal <76664798+Knowledge-JO@users.noreply.github.com>
2025-02-13 20:32:25 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -40,15 +40,17 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
-@router.get("/api_status", status_code=status.HTTP_200_OK)
-async def api_status():
-    return {"status": "Active"}
 
 @router.get(
     "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
 )
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+
+
+@router.get("/api_status", status_code=status.HTTP_200_OK)
+async def api_status():
+    return {"status": "Active"}
 
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
